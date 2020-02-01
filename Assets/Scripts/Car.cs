c06bbfc3ed043a6fcd646914a13eebcb3d6795b1
Guney Ozsan <guneyozsan@gmail.com>
2020-02-02 02:08:20 +0300

    Implement car repair action on mouse click.

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Car : MonoBehaviour
+{
+    private const int maxHealth = 100;
+
+    [SerializeField] private int healAmount;
+    [SerializeField] private int health;
+
+    private void Start() =>
+        Health = Random.Range(0, maxHealth);
+    
+    private int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+            OnHealthUpdated();
+        }
+    }
+
+    private void OnMouseUp() =>
+        Health += healAmount;
+
+    private void OnHealthUpdated()
+    {
+        if (Health < maxHealth)
+        {
+            return;
+        }
+
+        OnCarRepaired();
+    }
+
+    private void OnCarRepaired() =>
+        Destroy(gameObject);
+}
