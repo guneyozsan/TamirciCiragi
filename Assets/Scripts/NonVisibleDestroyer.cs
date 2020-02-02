029cc538517ccd2673d0af71beb52e18352a6396
Guney Ozsan <guneyozsan@gmail.com>
2020-02-02 15:04:48 +0300

    Destroy cars that cannot enter shop.

---
+++ Assets/Scripts/NonVisibleDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class NonVisibleDestroyer : MonoBehaviour
+{
+    [SerializeField] private float minDistanceY;
+
+    private Rigidbody2D rigidbody2;
+
+    private void Awake()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (rigidbody2.velocity == Vector2.zero && 
+            transform.localPosition.y < -1f * minDistanceY)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
