4de2afbbe63a7f14467b361bebe3ef36e7f11be6
Guney Ozsan <guneyozsan@gmail.com>
2020-02-02 11:40:42 +0300

    Decrease girl bar over time.

--- Assets/Scripts/Girl.cs
+++ Assets/Scripts/Girl.cs
@@ -4,29 +4,33 @@ public class Girl : MonoBehaviour
 {
     private const int maxLove = 100;
 
-    [SerializeField] private float deltaLove;
+    [SerializeField] private float loveIncreaseAmount;
+    [SerializeField] private float loveDecreaseRate;
     [SerializeField] private ProgressBar progressBar;
 
     private float love;
+
+    private void Start() =>
+        Love = 0;
+
+    private void FixedUpdate() =>
+        Love -= loveDecreaseRate * Time.deltaTime;
+
+    private void OnMouseUp()
+    {
+        float deltaLoveRatio = loveIncreaseAmount / (maxLove - Love);
+        Love += loveIncreaseAmount;
+        loveIncreaseAmount = deltaLoveRatio * (maxLove - Love);
+    }
 
     public float Love
     {
         get { return love; }
         set
         {
-            love = value;
+            love = Mathf.Max(0f, value);
             OnLoveChanged();
         }
-    }
-
-    private void Start() =>
-        Love = 0;
-
-    private void OnMouseUp()
-    {
-        float deltaLoveRatio = deltaLove / (maxLove - Love);
-        Love += deltaLove;
-        deltaLove = deltaLoveRatio * (maxLove - Love);
     }
 
     private void OnLoveChanged() =>
