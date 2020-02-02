93e0e5380ffe1c4b905c68210daf80de28fe7d7f
Guney Ozsan <guneyozsan@gmail.com>
2020-02-02 10:32:20 +0300

    Implement girl mechanic.

---
+++ Assets/Scripts/Girl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Girl : MonoBehaviour
+{
+    private const int maxLove = 100;
+
+    [SerializeField] private float deltaLove;
+
+    [SerializeField] private float love;
+
+    private void Start()
+    {
+        love = 0;
+    }
+
+    private void OnMouseUp()
+    {
+        float deltaLoveRatio = deltaLove / (maxLove - love);
+        love += deltaLove;
+        deltaLove = deltaLoveRatio * (maxLove - love);
+    }
+}
