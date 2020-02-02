1dfb1563ef283a13e015edaf6553a2048cbc2453
Guney Ozsan <guneyozsan@gmail.com>
2020-02-02 16:59:43 +0300

    Reload level on game over. Fix girl sprite picker.

---
+++ Assets/Scripts/LevelReloader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelReloader : MonoBehaviour
+{
+    [SerializeField] private Boss boss;
+
+    private void Awake() =>
+        boss.IsAngry += Boss_IsAngry;
+
+    private void Boss_IsAngry()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    private IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(3);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
